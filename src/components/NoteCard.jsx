257122bc11e0ffaf5c81c22138cbf8f79935af5a
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-17 11:32:50 -0700

    Added dynamic props support to NoteCard component

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
-const NoteCard = () => {
+const NoteCard = (props) => {
   return (
     <div className="bg-slate-800 p-6 rounded-2xl text-white max-w-2xl mt-10">
         <h2 className="text-2xl font-bold mb-4">
-            Recursion Notes
+            {props.title}
         </h2>
 
         <p className="text-slate-300 leading-8">
-             Recursion is a programming technique where a function calls itself
-        repeatedly to solve a problem.
+           {props.content}
         </p>
       
     </div>
