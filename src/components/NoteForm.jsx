831806adc824a0baee973d2afb202a2d39661adb
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 17:11:41 -0700

    Connected topic input with global context state

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
+import { useContext } from 'react';
+import NotesContext from '../context/NotesContext';
 
-const Noteform = () =>{
-    return (
-        <div className="bg-slate-800 p-6 rounded-2xl w-full max-w-2xl">
-            <input type="text"
-            placeholder="Enter study topic..." className="w-full p-4 rounded-xl bg-slate-900 text-white outline-none mb-5"/>
+const NoteForm = () => {
+  const { topic, setTopic } = useContext(NotesContext);
+  return (
 
-            <select className="w-full p-4 rounded-xl bg-slate-900 text-white outline-none">
-                <option>Beginner</option>
-                <option>Intermediate</option>
-                <option>Advanced</option>
-            </select>
-            <button className="bg-red-500 hover:bg-red-600 transition px-6 py-3 rounded-xl text-white mt-6 w-full">
-                Generate Notes
-            </button>
-        </div>
-    )
-}
+    <div className="bg-slate-800 p-6 rounded-2xl w-full max-w-2xl">
+
+      <input
+        type="text"
+        placeholder="Enter study topic..."
+        className="w-full p-4 rounded-xl bg-slate-900 text-white outline-none mb-5"
+        value={topic} onChange={(e) => setTopic(e.target.value)}
+      />
+
+      <select
+        className="w-full p-4 rounded-xl bg-slate-900 text-white outline-none"
+      >
+        <option>Beginner</option>
+        <option>Intermediate</option>
+        <option>Advanced</option>
+      </select>
+
+      <button
+        className="bg-red-500 hover:bg-red-600 transition px-6 py-3 rounded-xl text-white mt-6 w-full"
+      >
+        Generate Notes
+      </button>
+
+    </div>
+
+  );
+};
+
 export default NoteForm;
