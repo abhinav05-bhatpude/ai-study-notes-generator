95405d72965e913cd0eb4f3d100374a067ee0bec
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 17:20:14 -0700

    Improved dynamic placeholder behavior using context state

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -4,7 +4,7 @@ import NotesContext from '../context/NotesContext';
 
 const NoteCard = () => {
   const { topic, difficulty } = useContext(NotesContext);
-  
+
   return (
     <div className="bg-slate-800 p-6 rounded-2xl hover:scale-[1.02] hover:shadow-2xl transition duration-300">
         <h2 className="text-2xl font-bold mb-4">
@@ -12,7 +12,9 @@ const NoteCard = () => {
         </h2>
 
         <p className="text-slate-300 leading-8">
-           Difficulty Level: {difficulty}
+           {topic
+  ? `Difficulty Level: ${difficulty}`
+  : "Enter a topic to generate notes"}
         </p>
       
     </div>
