61b7e3bb6c849f2542a72a26bc50ebc8c5a201cf
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 17:07:10 -0700

    Implemented NotesProvider with shared topic and difficulty state

--- src/context/NotesContext.jsx
+++ src/context/NotesContext.jsx
@@ -1,5 +1,28 @@
-import { createContext } from "react";
+import { createContext,useState } from "react";
 
 const NotesContext=createContext();
 
+export const NotesProvider = ({children}) => {
+    
+    const [topic,setTopic] = useState("");
+
+    const [difficulty,setDifficulty] = useState("Beginner");
+
+    return (
+        <NotesContext.Provider
+        value={{
+            topic,
+            setTopic,
+            difficulty,
+            setDifficulty,
+        }}
+        >
+            {children}
+
+        </NotesContext.Provider>
+    );
+
+};
+
 export default NotesContext;
+
