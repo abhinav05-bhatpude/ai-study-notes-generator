b2eb4ab29aaa55c5b2963648be44e6a6c8e375bd
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-18 17:17:50 -0700

    Integrated NoteCard with shared context data

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { useState } from 'react';
+import NotesContext from '../context/NotesContext';
 
-const NoteCard = (props) => {
+const NoteCard = () => {
+  const { topic, difficulty } = useContext(NotesContext);
+  
   return (
     <div className="bg-slate-800 p-6 rounded-2xl hover:scale-[1.02] hover:shadow-2xl transition duration-300">
         <h2 className="text-2xl font-bold mb-4">
-            {props.title}
+            {topic || "Your Notes Will Appear Here"}
         </h2>
 
         <p className="text-slate-300 leading-8">
-           {props.content}
+           Difficulty Level: {difficulty}
         </p>
       
     </div>
