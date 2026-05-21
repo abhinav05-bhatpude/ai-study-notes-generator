c2a67dea23c1c7ea0ee0d876e1468094b6629e45
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-21 12:16:14 -0700

    changes Title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,12 @@ const Navbar = () => {
    <nav className="flex justify-between items-center mb-10">
 
     <h1 className="text-red-500 text-3xl font-bold">
-      AI Notes 🧠
+     AI Notes Generator 🤖
     </h1>
 
+<p className="text-slate-400 text-sm">
+  Powered by Gemini AI
+</p>
     <div className="flex gap-6 text-white">
     
     <Link to="/">Home</Link>
